a8a2c105405ac1f78e82c447ee931cec148daa95
Rajeev Sahasrabudhe <cardonline.india@gmail.com>
2023-11-09 11:04:58 +0530

    Chapter 9 complete

--- app.js
+++ app.js
@@ -1,41 +1,88 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
+//connect to mongo db
 const app = express();
+
+const dbURI='mongodb+srv://printonline1234:YwJCpxW6Roch3XOF@gurukul1.rxshjnf.mongodb.net/gurukul1?retryWrites=true&w=majority'
+mongoose.connect(dbURI)
+    .then((result) => console.log('connected to db...'))
+    .catch((err) => console.log(err));
 
 //register view engine
 app.set('view engine', 'ejs')
 
-// listen for requests
-
+// listen for requests is being done in mongoose. connect .then above here.
 app.listen(3000);
+
 
 // middleware & static files
 app.use(express.static('public'));
 
 app.use(morgan('dev'));
 
+// mongoose and mongo sandbox routes
 
+app.get('/add-blog', (req,res)=>{
+    const blog = new Blog({
+        title: 'new blog 2',
+        snippet:'about my new blog',
+        body: 'more about my new blog'
+    });
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+});
+
+app.get('/all-blogs', (req,res)=>{
+    Blog.find()
+        .then((result) =>{
+            res.send(result);
+        })
+        .catch((err) =>{
+           console.log(err); 
+        })
+})
+
+
+app.get('/single-blog', (req, res)=>{
+    Blog.findById('654b660c8ae7f2c3ca37d0c1')
+    .then((result) =>{
+        res.send(result);
+    })
+    .catch((err) =>{
+       console.log(err); 
+    })
+})
 app.get('/', (req, res)=>{
-    // res.send('<h1>Home page</h1>');
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'lorem epsum something...'},
-        {title: 'Seema does micro exercises', snippet: 'lorem epsum something...'},
-        {title: 'Rajeev goes to RSS shakha daily', snippet: 'lorem epsum something...'},
-        {title: 'Om is learning Hospitality', snippet: 'lorem epsum something...'},
-    ];
-    res.render('index',{title:"Home",blogs})
-    console.log('listening...')
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res)=>{
     // res.send('<h1>About page</h1>');
     res.render('about',{title:"About"})
-    console.log('listening...')
 });
 
+
+// blog routes
+app.get('/blogs', (req, res) =>{
+    Blog.find().sort({ createdAt: -1})
+        .then((result)=>{
+            res.render('index', {blogs:result ,title:'All blogs'});
+        })
+        .catch((err) =>{
+            console.log(err); 
+         })
+})
+
 // blogs create
-app.get('/blogs/create', (req, res)=>{
+app.get('/blogs-create', (req, res)=>{
     res.render('create',{title:"Create Blog"});
 });
 // 404 page
